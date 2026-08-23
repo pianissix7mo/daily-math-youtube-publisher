5ea620b377c3783cf301b1db48748654d95bcebf
Mofei Wang <70549770+pianissix7mo@users.noreply.github.com>
2026-08-23 15:50:21 -0400

    Use playlist-capable YouTube OAuth scope

--- scripts/run_schedule.py
+++ scripts/run_schedule.py
@@ -2,8 +2,7 @@
 import schedule_package
 
 schedule_package.base.SCOPES = [
-    "https://www.googleapis.com/auth/youtube.upload",
-    "https://www.googleapis.com/auth/youtube.readonly",
+    "https://www.googleapis.com/auth/youtube",
 ]
 
 if __name__ == "__main__":
